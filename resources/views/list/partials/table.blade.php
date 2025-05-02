65552fb60c7c2a429cbfa0d76d3471a17692241b
farshid amani <f.amani.dev@gmail.com>
2025-05-02 07:37:53 +0330

    sdf

--- resources/views/list/partials/table.blade.php
+++ resources/views/list/partials/table.blade.php
@@ -80,18 +80,18 @@
                         @endforeach
                     </td>
                     <td class="p-4">
-                        <div class="" style="margin-right: 10px; display: inline-block;"> <a href="#"
+                        <div class="" style="margin-right: 8px; display: inline-block;"> <a href="#"
                                 class="m-1 save-resident">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27"
                                     fill="currentColor" class="bi bi-play-fill text-success" viewBox="0 0 16 16">
                                     <path
                                         d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
                                 </svg>
                             </a></div>
-                        <div class=""style="margin-right: 10px; display: inline-block;"><a href="#"
+                        <div class=""style="margin-right: 8px; display: inline-block;"><a href="#"
                                 class="m-1" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop"
                                 aria-controls="staticBackdrop">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27"
                                     fill="currentColor" class="bi bi-pencil-square text-info" viewBox="0 0 16 16">
                                     <path
                                         d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -99,10 +99,10 @@
                                         d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                                 </svg>
                             </a></div>
-                        <div class=""style="margin-left: 10px; display: inline-block;"><a href="#"
+                        <div class=""style="margin-left: 5px; display: inline-block;"><a href="#"
                                 class="m-1" id="modalDesc" data-bs-toggle="offcanvas" data-bs-target="#modalDesc"
                                 aria-controls="modalDesc">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27"
                                     fill="currentColor" class="bi bi-brush-fill text-primary" viewBox="0 0 16 16">
                                     <path
                                         d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.1 6.1 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.1 8.1 0 0 1-3.078.132 4 4 0 0 1-.562-.135 1.4 1.4 0 0 1-.466-.247.7.7 0 0 1-.204-.288.62.62 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896q.19.012.348.048c.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04" />
